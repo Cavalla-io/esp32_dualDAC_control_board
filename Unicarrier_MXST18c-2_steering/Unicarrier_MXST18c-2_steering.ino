eeb891f45345396094edc85a0180e7cfdfd65dc3
Victor <victor.w.boyd@gmail.com>
2025-05-05 16:00:51 -0700

    no remote start and serial peek functionality (still fails on command)

--- Unicarrier_MXST18c-2_steering/Unicarrier_MXST18c-2_steering.ino
+++ Unicarrier_MXST18c-2_steering/Unicarrier_MXST18c-2_steering.ino
@@ -2,7 +2,6 @@
 
 #define MCP4921_CS1_PIN  5  // Chip Select for DAC 1
 #define MCP4921_CS2_PIN  4  // Chip Select for DAC 2
-#define REMOTE_START_PIN 2 // Remote start pin
 
 float voltage1 = 1.4;
 float voltage2 = 3.4;
@@ -17,10 +16,8 @@ void setup()
 
   pinMode(MCP4921_CS1_PIN, OUTPUT);
   pinMode(MCP4921_CS2_PIN, OUTPUT);
-  pinMode(REMOTE_START_PIN, OUTPUT);
   digitalWrite(MCP4921_CS1_PIN, HIGH);
   digitalWrite(MCP4921_CS2_PIN, HIGH);
-  digitalWrite(REMOTE_START_PIN, LOW);  // Initialize to OFF state
 
   SPI.begin(18, -1, 23, -1);  // Initialize VSPI
   setDACVoltage(MCP4921_CS1_PIN, voltage1);
@@ -45,31 +42,33 @@ void setDACVoltage(uint8_t csPin, float voltage)
 
 void loop()
 {
-  // Check for keyboard control via Serial
-  if (Serial.available() > 0) {
-    char command = Serial.read();
-    
-    // Check for the 'i' character to identify device
-    if (command == 'i') {
+  // Check for serial commands
+  if (Serial.available()) {
+    // Check for single character commands first
+    if (Serial.peek() == 'i') {
+      Serial.read(); // Consume the 'i'
       Serial.println("steering");
     }
-    else if (command == 'c') { // set remote start to close (forklift is on)
-      digitalWrite(REMOTE_START_PIN, HIGH);
-    } else if (command == 'o') { // set remote start to open (forklift is off)
-      digitalWrite(REMOTE_START_PIN, LOW);
-    } else if (command == 'r') {  // Manual steering reversal
+    else if (Serial.peek() == 'r') {
+      Serial.read(); // Consume the 'r'
       increasing1 = !increasing1;
       increasing2 = !increasing2;
-    } else if (command == 'p') {  // Key pressed -> Start voltage updates
+    }
+    else if (Serial.peek() == 'p') {
+      Serial.read(); // Consume the 'p'
       shouldUpdate = true;
-    } else if (command == 's') {  // Key released -> Stop voltage updates
+    }
+    else if (Serial.peek() == 's') {
+      Serial.read(); // Consume the 's'
       shouldUpdate = false;
     }
+    else {
+      // If not a single character command, clear the buffer
+      while (Serial.available()) {
+        Serial.read();
+      }
+    }
   }
-
-  // Always set the current voltages to maintain them
-  setDACVoltage(MCP4921_CS1_PIN, voltage1);
-  setDACVoltage(MCP4921_CS2_PIN, voltage2);
 
   // Only update voltage values if a key is being held
   if (shouldUpdate) {
@@ -91,4 +90,8 @@ void loop()
       if (voltage2 <= 0.55) increasing2 = true;
     }
   }
+
+  // Always set the current voltages to maintain them
+  setDACVoltage(MCP4921_CS1_PIN, voltage1);
+  setDACVoltage(MCP4921_CS2_PIN, voltage2);
 }
